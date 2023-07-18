9a3c22cb3b3eea418c91e6d2000798cf0ccc4ed8
ReeshU Raj <31492200+rajreeshu@users.noreply.github.com>
2023-07-18 12:39:03 +0530

    Update Testing.java
    commit from github

--- src/main/java/com/digit/testproject/Testing.java
+++ src/main/java/com/digit/testproject/Testing.java
@@ -4,7 +4,6 @@ public class Testing {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello world");
-		System.out.println("Hello world");
 		System.out.println("bhk");
 		System.out.println("bhk");
 	}
