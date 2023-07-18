a95e1d3db5d321e518079b9f4381c13d7c7c4fd0
Reeshu.Raj <Reeshu.Raj@LDIGITBGL-L0135.godigit.corp>
2023-07-18 12:23:39 +0530

    This is my first commit

---
+++ src/main/java/com/digit/testproject/Testing.java
@@ -0,0 +1,9 @@
+package com.digit.testproject;
+
+public class Testing {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+	}
+}
